598296bc16fcf0e5a1c38588611ecf897ef4c882
BRUTEFORCE-HACKER <84610202+BRUTEFORCE-HACKER@users.noreply.github.com>
2024-05-02 20:43:50 +0800

    Update still_alive.js

--- still_alive.js
+++ still_alive.js
@@ -1,12 +1,1 @@
-const express = require('express');
-const app = express();
 
-const port = process.env.PORT || 5000
-
-//test test
-
-app.get('/', ( req, res) => res.send('Modified for botpack JOEL'));
-
-app.listen(port, () =>
-	console.log(`Your app is listening a http://localhost:${port}`)
-);
